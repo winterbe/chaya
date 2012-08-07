3d8ab3bc73079deea21a1f2e71e7cb2b24869994
Benjamin Winterberg <winterbe@googlemail.com>
2012-08-07 19:49:18 +0200

    refactoring: introduce modules for ContentArea and Sidebar

--- src/public/chaya.js
+++ src/public/chaya.js
@@ -1,90 +1,84 @@
 $(function () {
     "use strict";
 
-    window.setInterval(updateTimes, 60000);
-
     // use mustache style templates
     _.templateSettings = {
         interpolate:/\{\{(.+?)\}\}/g
     };
 
-    var debug = false;
 
-    if (debug) {
-        connect('benjamin');
-    }
+    var ContentArea = function() {
+        var messageTemplate = _.template('<div class="box">\n    <div class="pic"></div>\n    <div class="msg">\n        <div class="meta">\n            <div class="from">{{nickname}}</div>\n            <div class="time" data-timestamp="{{timestamp}}"></div>\n        </div>\n        <div class="content">{{message}}</div>\n    </div>\n</div>');
+        var metaTemplate = _.template('<div class="info"><i class="icon-bell"></i>&nbsp;&nbsp;{{message}}</div>');
 
-    var messageTemplate = _.template('<div class="box">\n    <div class="pic"></div>\n    <div class="msg">\n        <div class="meta">\n            <div class="from">{{nickname}}</div>\n            <div class="time" data-timestamp="{{timestamp}}"></div>\n        </div>\n        <div class="content">{{message}}</div>\n    </div>\n</div>');
-    var metaTemplate = _.template('<div class="info"><i class="icon-bell"></i>&nbsp;&nbsp;{{message}}</div>');
-    var userTemplate = _.template('<li data-nickname="{{nickname}}"><i class="icon-user"></i> {{nickname}}</li>');
+        function appendMessage(data) {
+            var $entry = $(messageTemplate(data));
+            var $time = $entry.find('.time');
+            updateTime($time);
+            $('#content').append($entry);
+            scrollDown();
+        }
 
-    function appendMessage(data) {
-        var $entry = $(messageTemplate(data));
-        var $time = $entry.find('.time');
-        updateTime($time);
-        $('#content').append($entry);
-        scrollDown();
-    }
+        function appendInfo(data) {
+            var $entry = $(metaTemplate(data));
+            var $time = $entry.find('.time');
+            updateTime($time);
+            $('#content').append($entry);
+            scrollDown();
+        }
 
-    function appendMeta(data) {
-        var $entry = $(metaTemplate(data));
-        var $time = $entry.find('.time');
-        updateTime($time);
-        $('#content').append($entry);
-        scrollDown();
-    }
+        function updateTimes() {
+            $('.time').each(function () {
+                updateTime($(this));
+            });
+        }
 
-    function addUserToSidebar(data) {
-        var $li = $(userTemplate(data));
-        $('#east ul').append($li);
-    }
+        function updateTime($time) {
+            var timestamp = $time.data('timestamp');
+            var fromNow = moment(timestamp).fromNow();
+            $time.text(fromNow);
+        }
 
-    function removeUserFromSidebar(nickname) {
-        $('#east li[data-nickname='+nickname+']').remove();
-    }
+        function scrollDown() {
+            $('html, body').prop('scrollTop', $(document).height());
+        }
 
-    function updateTimes() {
-        $('.time').each(function() {
-            updateTime($(this));
-        });
-    }
+        return {
+            appendMessage:appendMessage,
+            appendInfo:appendInfo,
+            updateTimes:updateTimes
+        };
+    }();
 
-    function updateTime($time) {
-        var timestamp = $time.data('timestamp');
-        var fromNow = moment(timestamp).fromNow();
-        $time.text(fromNow);
-    }
 
-    function scrollDown() {
-        $('html, body').prop('scrollTop', $(document).height());
-    }
+    var Sidebar = function() {
+        var userTemplate = _.template('<li data-nickname="{{nickname}}"><i class="icon-user"></i> {{nickname}}</li>');
 
-    $('#nickname input')
-        .focus()
-        .on('keyup', function (ev) {
-            if (ev.keyCode === 13) {
-                var nickname = $(this).val();
-                if (nickname) {
-                    connect(nickname);
-                }
-            }
-        });
+        function addUser(data) {
+            var $li = $(userTemplate(data));
+            $('#east ul').append($li);
+        }
+
+        function removeUser(nickname) {
+            $('#east li[data-nickname=' + nickname + ']').remove();
+        }
+
+        return {
+            addUser:addUser,
+            removeUser:removeUser
+        };
+    }();
+
 
     function connect(nickname) {
         var socket = io.connect('/');
 
-        socket.on('poke', appendMessage);
-        socket.on('meta', appendMeta);
-        socket.on('user-connected', addUserToSidebar);
-        socket.on('user-disconnected', removeUserFromSidebar);
+        socket.on('poke', ContentArea.appendMessage);
+        socket.on('meta', ContentArea.appendInfo);
+        socket.on('user-connected', Sidebar.addUser);
+        socket.on('user-disconnected', Sidebar.removeUser);
 
         socket.emit('whoami', nickname);
-
-
-        if (debug) {
-            socket.emit('peek', 'was geht ab?');
-        }
-
 
         $('#nickname').hide();
         $('#chat').show();
@@ -102,4 +96,18 @@ $(function () {
             });
 
     }
+
+
+    $('#nickname input')
+        .focus()
+        .on('keyup', function (ev) {
+            if (ev.keyCode === 13) {
+                var nickname = $(this).val();
+                if (nickname) {
+                    connect(nickname);
+                }
+            }
+        });
+
+    window.setInterval(ContentArea.updateTimes, 60000);
 });
