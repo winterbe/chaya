20f61b5d94d897201f8257ae2154a302816e3af1
Benjamin Winterberg <winterbe@googlemail.com>
2012-08-11 12:55:57 +0200

    show user count in title

--- src/public/chaya.js
+++ src/public/chaya.js
@@ -15,7 +15,7 @@ $(function () {
 
     var Templates = {
         main:_.template('<div id="chaya">\n    <div class="titlebar">\n        <div class="brand" title="{{subtitle}}">{{title}}</div>\n    </div>\n\n    <div class="sidebar">\n        <h3>CONNECTED USERS</h3>\n        <ul></ul>\n    </div>\n\n    <div class="content"></div>\n\n    <div class="actionbar">\n        <div class="pic"></div>\n        <div class="wrapper">\n            <input type="text" placeholder="Leave a message...">\n        </div>\n    </div>\n</div>'),
-        connect:_.template('<div id="connect">\n    <h1>{{title}}</h1>\n\n    <h3>{{subtitle}}</h3>\n    <input type="text" placeholder="Choose your nickname">\n</div>\n\n<a id="ribbon" href="https://github.com/winterbe/chaya">\n    <img class="github-ribbon"\n         src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub">\n</a>\n\n<div class="footer">\n    <span class="copyright">© 2012 <a href="https://twitter.com/benontherun">Benjamin Winterberg</a></span>\n</div>'),
+        connect:_.template('<div id="connect">\n    <h1>{{title}}</h1>\n\n    <h3>{{subtitle}}</h3>\n    <input type="text" placeholder="Choose your nickname">\n</div>\n\n<a id="ribbon" href="https://github.com/winterbe/chaya">\n    <img class="github-ribbon"\n         src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub">\n</a>\n\n<div id="footer">\n    <span class="copyright">© 2012 <a href="https://twitter.com/benontherun">Benjamin Winterberg</a></span>\n</div>'),
         chatMessage:_.template('<div class="box">\n    <div class="pic"></div>\n    <div class="msg">\n        <div class="meta">\n            <div class="from">{{nickname}}</div>\n            <div class="time" data-timestamp="{{timestamp}}"></div>\n        </div>\n        <div class="chat-msg">{{message}}</div>\n    </div>\n</div>'),
         chatInfo:_.template('<div class="info"><i class="icon-bell"></i>&nbsp;&nbsp;{{message}}</div>'),
         userEntry:_.template('<li data-nickname="{{nickname}}"><i class="icon-user"></i> {{nickname}}</li>')
@@ -66,14 +66,31 @@ $(function () {
 
     var Sidebar = function () {
 
+        var userCount = 0;
+
         function addUser(data) {
+            userCount++;
             var $li = $(Templates.userEntry(data));
             $('#chaya .sidebar ul').append($li);
+            updateTitle();
         }
 
         function removeUser(nickname) {
+            userCount--;
             $('#chaya .sidebar li[data-nickname=' + nickname + ']').remove();
+            updateTitle();
         }
+
+        function updateTitle() {
+            var title = '';
+            if (userCount > 0) {
+                title = '[' + userCount + '] ';
+            }
+            title += Settings.title + ' - ' + Settings.subtitle;
+            document.title = title;
+        }
+
+        updateTitle();
 
         return {
             addUser:addUser,
@@ -101,6 +118,7 @@ $(function () {
 
         window.setTimeout(function () {
             $('#ribbon').hide();
+            $('#footer').hide();
             $('#connect').hide();
             $('#chaya').show().find('input').focus();
         }, 800);
